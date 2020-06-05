534547b181b2b6fad3b7e5950d67a75cd8975ecd
Pier <poggionni@atlantic-technologies.com>
2020-06-05 16:35:43 +0200

    success

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="jumbotron">
- <h1>PHP Graph Tutorial</h1>
- <p class="lead">This sample app shows how to use the Microsoft Graph API to access a user's data from PHP</p>
+ <h1>test microsoft graph</h1>
+ <p class="lead">test app for microsoft graph api</p>
  @if(isset($userName))
    <h4>Welcome {{ $userName }}!</h4>
-   <p>Use the navigation bar at the top of the page to get started.</p>
+   
  @else
    <a href="/signin" class="btn btn-primary btn-large">Click here to sign in</a>
  @endif
